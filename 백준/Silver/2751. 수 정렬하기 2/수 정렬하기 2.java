3c0b0ba8a2d2b52de37c3a1f81a93d99bce7e0be
DongGeon <103568083+sondg921@users.noreply.github.com>
2025-03-21 21:53:42 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2588 ms, Memory: 334576 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+        
+        for (int i : list) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
